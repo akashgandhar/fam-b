6abdd67ab1793e744dc11f7c01b7f7651672151f
akashgandhar <akashgandhar007@gmail.com>
2024-10-04 15:56:04 +0530

    check

--- models/FrameNumber.model.js
+++ models/FrameNumber.model.js
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
 
 const frameNumberSchema = new mongoose.Schema({
-    imageUrl: String,
-    name: String,
-    numberOfFrames: Number,
-    price: Number,
-    comparePrice: Number,
-    description: String,
-    description2: String,
-    additionalImages: [Object],
-    type: String,
+    'imageUrl': String,
+    'name': String,
+    'numberOfFrames': Number,
+    'price': Number,
+    'comparePrice': Number,
+    'description': String,
+    'description2': String,
+    'additionalImages': [Object],
+    'type': String,
 }, { timestamps: true });
 
 const FrameNumber = mongoose.model('FrameNumber', frameNumberSchema); // Create the model
