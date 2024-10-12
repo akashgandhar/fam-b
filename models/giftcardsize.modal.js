00e5c89a624a4bb8f507bd2364bf5e98807ef793
Akash Gandhar <76213112+akashgandhar@users.noreply.github.com>
2024-10-12 14:35:57 +0530

    Update giftcardsize.modal.js

--- models/giftcardsize.modal.js
+++ models/giftcardsize.modal.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const GiftCardSizeSchema = new mongoose.Schema({
     size: { type: String, required: true }, 
+    price: { type: Number, required: true },
 }, { timestamps: true });
 
 const GiftCardSize = mongoose.model('GiftCardSize', GiftCardSizeSchema);
