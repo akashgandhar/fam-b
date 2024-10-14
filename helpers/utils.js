18b3fc31167e7555b95509d442878dbe8dba4456
Akash Gandhar <76213112+akashgandhar@users.noreply.github.com>
2024-10-14 19:23:43 +0530

    Update utils.js

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,142 +1,169 @@
-import mongoose from 'mongoose';
-import JWT from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import moment from 'moment';
-import { createHash } from 'crypto';
-
+import mongoose from "mongoose";
+import JWT from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import moment from "moment";
+import { createHash } from "crypto";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-const generateToken = payload => JWT.sign(payload, JWT_SECRET);
+const generateToken = (payload) => JWT.sign(payload, JWT_SECRET);
 
-const verifyToken = token => {
-    try {
-        return JWT.verify(token, JWT_SECRET)
-    } catch (error) {
-        return false;
-    }
+const verifyToken = (token) => {
+  try {
+    return JWT.verify(token, JWT_SECRET);
+  } catch (error) {
+    return false;
+  }
 };
 
 const escapeSpecialCharacter = (text) => {
-    if (text) return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-    else return '';
-}
+  if (text) return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+  else return "";
+};
 
 const encodeRequest = (payload) => {
-    return Buffer.from(JSON.stringify(payload)).toString("base64");
-}
+  return Buffer.from(JSON.stringify(payload)).toString("base64");
+};
 
 const signRequest = (payload) => {
-    return createHash("sha256").update(payload).digest("hex");
-}
+  return createHash("sha256").update(payload).digest("hex");
+};
 
 const decodeStringToBase64 = (payload) => {
-    return JSON.parse(Buffer.from(payload, 'base64'));
-}
+  return JSON.parse(Buffer.from(payload, "base64"));
+};
 
-const hashPassword = async password => await bcrypt.hash(password, 6);
+const hashPassword = async (password) => await bcrypt.hash(password, 6);
 
-const comparePassword = async (password1, password2) => await bcrypt.compare(password1, password2);
+const comparePassword = async (password1, password2) =>
+  await bcrypt.compare(password1, password2);
 
-const isPhoto = mimetype => mimetype && mimetype == 'image/jpeg' || mimetype == 'image/jpg' || mimetype == 'image/png' || mimetype == 'image/gif';
+const isPhoto = (mimetype) =>
+  (mimetype && mimetype == "image/jpeg") ||
+  mimetype == "image/jpg" ||
+  mimetype == "image/png" ||
+  mimetype == "image/gif";
 
-const isVideo = mimetype => {
-    if (!mimetype) return false;
-    switch (mimetype) {
-        case 'video/mp4':
-        case 'video/quicktime':
-        case 'video/mpeg':
-        case 'video/mp2t':
-        case 'video/webm':
-        case 'video/ogg':
-        case 'video/x-ms-wmv':
-        case 'video/x-msvideo':
-        case 'video/3gpp':
-        case 'video/3gpp2':
-            return true;
-        default:
-            return false;
-    }
-}
+const isVideo = (mimetype) => {
+  if (!mimetype) return false;
+  switch (mimetype) {
+    case "video/mp4":
+    case "video/quicktime":
+    case "video/mpeg":
+    case "video/mp2t":
+    case "video/webm":
+    case "video/ogg":
+    case "video/x-ms-wmv":
+    case "video/x-msvideo":
+    case "video/3gpp":
+    case "video/3gpp2":
+      return true;
+    default:
+      return false;
+  }
+};
 
 const paginationData = (totalCount, LIMIT, OFFSET) => {
-    let totalPages = Math.ceil(totalCount / LIMIT);
-    let currentPage = Math.floor(OFFSET / LIMIT);
-    let prevPage = (currentPage - 1) > 0 ? (currentPage - 1) * LIMIT : 0;
-    let nextPage = (currentPage + 1) <= totalPages ? (currentPage + 1) * LIMIT : 0;
+  let totalPages = Math.ceil(totalCount / LIMIT);
+  let currentPage = Math.floor(OFFSET / LIMIT);
+  let prevPage = currentPage - 1 > 0 ? (currentPage - 1) * LIMIT : 0;
+  let nextPage = currentPage + 1 <= totalPages ? (currentPage + 1) * LIMIT : 0;
 
-    return {
-        totalCount,
-        nextPage,
-        prevPage,
-        totalCount,
-        currentPage: currentPage + 1
-    }
-}
+  return {
+    totalCount,
+    nextPage,
+    prevPage,
+    totalCount,
+    currentPage: currentPage + 1,
+  };
+};
 
-const createSuccessResponse = (message, data = null, success = true) => {
-    return { success, message, data };
-}
+const createSuccessResponse = async (message, data = null, success = true) => {
+  const order = await new OrderModel2({
+    cart: [product], // Assuming a single product for now
+    address: {
+      email: address.email || "",
+      name: address.name || "",
+      lastName: address.lastName || "",
+      city: address.city || "",
+      pincode: address.pincode || "",
+      state: address.state || "",
+      phone: address.phone || "",
+      country: address.country || "",
+      street: address.street || "",
+    },
+    paymentType,
+  }).save();
+
+  console.log("order", order);
+  console.log("Initiating shipment...");
+  console.log("Order created:", order);
+  console.log(
+    "................>>>>>>>............................................."
+  );
+
+  return { success, message, data };
+};
 
 const createErrorResponse = (message, data = null, success = false) => {
-    return { success, message, data };
-}
+  return { success, message, data };
+};
 
-const parseToMongoObjectID = string => mongoose.Types.ObjectId(string);
+const parseToMongoObjectID = (string) => mongoose.Types.ObjectId(string);
 
 const uuidv4_10 = () => {
-    let dt = new Date().getTime();
-    return 'xxxyxxyxxyxx'.replace(/[xy]/g, c => {
-        let r = (dt + Math.random() * 16) % 16 | 0;
-        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16).toUpperCase();
-    });
-}
+  let dt = new Date().getTime();
+  return "xxxyxxyxxyxx".replace(/[xy]/g, (c) => {
+    let r = (dt + Math.random() * 16) % 16 | 0;
+    return (c == "x" ? r : (r & 0x3) | 0x8).toString(16).toUpperCase();
+  });
+};
 
 const generateOrderId = () => {
-    let dt = new Date().getTime();
-    return 'xxxyxxyxxy'.replace(/[xy]/g, c => {
-        let r = (dt + Math.random() * 16) % 16 | 0;
-        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16).toUpperCase();
-    });
-}
+  let dt = new Date().getTime();
+  return "xxxyxxyxxy".replace(/[xy]/g, (c) => {
+    let r = (dt + Math.random() * 16) % 16 | 0;
+    return (c == "x" ? r : (r & 0x3) | 0x8).toString(16).toUpperCase();
+  });
+};
 
 const generatePromoCode = () => {
-    let dt = new Date().getTime();
-    return 'yxxyxy'.replace(/[xy]/g, c => {
-        let r = (dt + Math.random() * 16) % 16 | 0;
-        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16).toUpperCase();
-    });
-}
+  let dt = new Date().getTime();
+  return "yxxyxy".replace(/[xy]/g, (c) => {
+    let r = (dt + Math.random() * 16) % 16 | 0;
+    return (c == "x" ? r : (r & 0x3) | 0x8).toString(16).toUpperCase();
+  });
+};
 
-const imageExtension = ['jpg', 'png', 'jpeg']
-const videoExtension = ['mp4', 'mkv', 'avi']
-const fileExtension = ['.jpg', '.png', '.jpeg', '.mp4', '.mkv', '.avi']
+const imageExtension = ["jpg", "png", "jpeg"];
+const videoExtension = ["mp4", "mkv", "avi"];
+const fileExtension = [".jpg", ".png", ".jpeg", ".mp4", ".mkv", ".avi"];
 
-const isObjectId = (id) => mongoose.Types.ObjectId.isValid(id)
+const isObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
 
 const generateOtp = () => Math.floor(1000 + Math.random() * 9000);
 
 export {
-    generateToken,
-    verifyToken,
-    hashPassword,
-    comparePassword,
-    isPhoto,
-    isVideo,
-    paginationData,
-    createSuccessResponse,
-    createErrorResponse,
-    parseToMongoObjectID,
-    uuidv4_10,
-    imageExtension,
-    videoExtension,
-    fileExtension,
-    isObjectId,
-    escapeSpecialCharacter,
-    generateOrderId,
-    generatePromoCode,
-    generateOtp,
-    encodeRequest,
-    signRequest,
-    decodeStringToBase64
-}
+  generateToken,
+  verifyToken,
+  hashPassword,
+  comparePassword,
+  isPhoto,
+  isVideo,
+  paginationData,
+  createSuccessResponse,
+  createErrorResponse,
+  parseToMongoObjectID,
+  uuidv4_10,
+  imageExtension,
+  videoExtension,
+  fileExtension,
+  isObjectId,
+  escapeSpecialCharacter,
+  generateOrderId,
+  generatePromoCode,
+  generateOtp,
+  encodeRequest,
+  signRequest,
+  decodeStringToBase64,
+};
