98f5b9e7612d4ec42830a5a9288d5c7b60e13c71
akashgandhar <akashgandhar007@gmail.com>
2024-10-20 16:54:04 +0530

    check pay

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ import { url } from "./helpers/backendUrl.js";
 import GiftCardSize from "./models/giftcardsize.modal.js";
 import GIftCard from "./models/giftcard.modal.js";
 import { log } from "console";
+import axios from "axios";
 
 cloudinary.config({
   cloud_name: "dk2qptwnw",
@@ -1600,6 +1601,115 @@ app.post("/promo/orderId", verifyUserJWTToken, async (req, res) => {
       .status(statusCode.error)
       .json(createErrorResponse(messages.promoNotFound));
 });
+
+
+
+app.post('/order', async (req, res) => {
+
+  try {
+
+      let merchantTransactionId = req.body.transactionId
+
+      const data = {
+          merchantId: process.env.live_phonePayMerchant,
+          merchantTransactionId: merchantTransactionId,
+          name: req.body.name,
+          amount: req.body.amount * 100,
+          redirectUrl: `http://localhost:8000/status?id=${merchantTransactionId}`,
+          redirectMode: "POST",
+          mobileNumber: req.body.phone,
+          paymentInstrument: {
+              type: "PAY_PAGE"
+          }
+      }
+
+
+      const payload = JSON.stringify(data)
+      const payloadMain = Buffer.from(payload).toString('base64')
+      const keyIndex = 1
+      const string = payloadMain + '/pg/v1/pay' + process.env.live_phonePaySaltKey;
+      const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+      const checksum = sha256 + '###' + keyIndex;
+
+
+      // const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+      const prod_URL = process.env.live_phonePayUrl
+
+      const options = {
+          method: 'POST',
+          url: prod_URL,
+          headers: {
+              accept: 'application/json',
+              'Content-Type': 'application/json',
+              'X-VERIFY': checksum
+          },
+          data: {
+              request: payloadMain
+          }
+      }
+
+      await axios(options).then(function (response) {
+
+          console.log(response.data)
+          return res.json(response.data)
+
+      }).catch(function (error) {
+          console.log(error)
+      })
+
+
+
+
+  } catch (error) {
+      console.log(error)
+  }
+
+
+})
+
+
+app.post('/status', async (req, res) => {
+
+  const merchantTransactionId = req.query.id
+  const merchantId = process.env.live_phonePayMerchant
+
+
+  const keyIndex = 1
+  const string = `/pg/v1/status/${merchantId}/${merchantTransactionId}` + process.env.live_phonePaySaltKey;
+  const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+  const checksum = sha256 + '###' + keyIndex;
+
+
+  const options = {
+      method: 'GET',
+      url: `https://api.phonepe.com/apis/hermes/pg/v1/status/${merchantId}/${merchantTransactionId}`,
+      headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+          'X-VERIFY': checksum,
+          'X-MERCHANT-ID': `${merchantId}`
+      }
+
+
+  }
+
+
+  axios.request(options).then(function (response) {
+      if (response.data.success === true) {
+          const url = 'http://localhost:5173/success'
+          return res.redirect(url)
+      } else {
+          const url = 'http://localhost:5173/fail'
+          return res.redirect(url)
+      }
+
+  }).catch(function (error) {
+      console.log(error)
+  })
+
+
+})
+
 
 server.listen(PORT, () => {
   console.log(`Server is listing on PORT ${PORT}`);
