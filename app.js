f57b0b8946402747fc892147756c466e6aa8fa53
akashgandhar <akashgandhar007@gmail.com>
2024-10-23 09:08:30 +0530

    added cors

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ import fs from "fs";
 
 const PORT = process.env.PORT;
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['https://familyvibes.in', 'https://admin.familyvibes.in'] // Replace with the allowed domain
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"))
