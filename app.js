40374196e005b0b731408f13a4f27ba23c3a15e7
akashgandhar <akashgandhar007@gmail.com>
2024-10-23 09:03:12 +0530

    no cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ import fs from "fs";
 
 const PORT = process.env.PORT;
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"))
